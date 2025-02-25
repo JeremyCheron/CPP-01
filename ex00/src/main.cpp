81acac9a5b458477305f005019f09111a9c9bbe0
Jeremy Cheron <jcheron@student.42lehavre.fr>
2025-02-25 10:56:40 +0100

    ~|update ex00 outputs ti be more specific

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcheron <jcheron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 14:40:32 by jcheron           #+#    #+#             */
-/*   Updated: 2025/02/25 09:39:50 by jcheron          ###   ########.fr       */
+/*   Updated: 2025/02/25 10:55:03 by jcheron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,28 @@ void	randomChump(std::string name);
 int main(void)
 {
 	// Zombie zombie;
+	std::cout << "Creating a stack zombie named Toto" << std::endl
+		<< "it will be automatically deleted at the end of execution" << std::endl;
 	Zombie zombie("Toto");
+	zombie.announce();
 
 	// Zombie in the stack
+	std::cout << "Creating a stack zombie named stack" << std::endl
+		<< "it will be automatically deleted because created in function scope" << std::endl;
 	randomChump("stack");
 
 	// Zombie announce
-	zombie.announce();
 
 	// Zombie in the heap
+	std::cout << "Creating a heap zombie named Heap" << std::endl
+		<< "it will be manually deleted" << std::endl;
 	Zombie *heap;
 	heap = newZombie("Heap");
 	heap->announce();
 	delete(heap);
 
+	std::cout << "Creating a heap zombie named Toto2" << std::endl
+		<< "it will be automatically deleted at the end of execution" << std::endl;
 	Zombie zombie2("Toto2");
 	zombie2.announce();
 
